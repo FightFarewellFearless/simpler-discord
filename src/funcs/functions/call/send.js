da4c19de6e7df4e56ca44af3389c748ef85789e1
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-04 05:40:38 +0700

    Update send.js

--- src/funcs/functions/call/send.js
+++ src/funcs/functions/call/send.js
@@ -4,13 +4,13 @@ usage: "[channel id;message;return id(yes/no)]",
 description: "send message in provided channel id",
 code: async (d) => {
    let [id, msg, returns] = d.data.splits;
-   const client = d.client;
-   let channel = await client.channels.cache.get(id);
-   if(!channel) channel = await client.channels.fetch(id, {force: true});
+  return ` const client = ${d.client};
+   let channel = await client.channels.cache.get(${id});
+   if(!channel) channel = await client.channels.fetch(${id}, {force: true});
    if(!channel) return d.sendError(d, "Invalid channel id provided");
    let mess;
    try {
- mess = await channel.send(d.util.embedParser(msg));
+ mess = await channel.send(${d.util.embedParser(msg)});
   }
 catch(e) {
 return d.sendError(d, e)
@@ -19,5 +19,5 @@ return d.sendError(d, e)
 
  }
 
-
+`
 }
